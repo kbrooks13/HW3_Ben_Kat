1abfbacf611f5c6a572c266790bd82ff448c1327
Ben Sacco <bsacco13@aol.com>
2018-03-30 22:13:02 -0400

    i tried to make a graph

--- Hw_scripts/hw_script_1.R
+++ Hw_scripts/hw_script_1.R
@@ -3,3 +3,8 @@ library(tidyverse)
 library(ggplot2)
 head(surveys)
 library(gridExtra)
+survey_2 <- select(surveys, plot_id, species_id)
+ggplot(survey_2, aes(x=species_id, y=plot_id))+
+  geom_point(alpha=.01)
+rodents <- select(surveys, taxa, hindfoot_length, weight) %>%
+  select(surveys, taxa=Rodent)
