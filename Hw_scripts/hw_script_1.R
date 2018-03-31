9075796e6d81aa191a3eaee2d66eaf3e6ec2bc79
Kathryn Brooks <kathrynb13@gmail.com>
2018-03-30 22:12:56 -0400

    A bad boxplot

--- Hw_scripts/hw_script_1.R
+++ Hw_scripts/hw_script_1.R
@@ -3,3 +3,6 @@ library(tidyverse)
 library(ggplot2)
 head(surveys)
 library(gridExtra)
+ggplot(surveys,
+                             aes(x=species_id, y=weight))+
+  geom_boxplot() + xlab("Species") + ylab("weight")
