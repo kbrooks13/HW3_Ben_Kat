ca0a72f39777b8be5dedca17153c5e1ae535a341
Ben Sacco <bsacco13@aol.com>
2018-04-02 13:34:15 -0400

    Graph made, r markdown file started

--- Hw_scripts/hw_script_1.R
+++ Hw_scripts/hw_script_1.R
@@ -14,3 +14,21 @@ ggplot(survey_2, aes(x=species_id, y=plot_id))+
 rodents <- select(surveys, taxa, hindfoot_length, weight) %>%
   select(surveys, taxa=Rodent)
 
+rodent_surveys <- select(surveys, taxa, sex, weight)
+head(rodent_surveys)
+
+rodent_surveys2 <- rodent_surveys %>% filter(!is.na(weight))
+rodent_surveys <- rodent_surveys2 %>% filter (!is.na(sex))                     
+
+rodent_surveys <- rodent_surveys %>% filter(taxa == "Rodent")
+rodent_sex_weight <- rodent_surveys %>%
+  group_by(sex) %>%
+  summarise(mean_weight= round(mean(weight),digits=2))
+
+mean_weight_bysex <- ggplot(rodent_sex_weight, aes(x=sex, y=mean_weight)) +
+  geom_col()+xlab("SEX") + ylab("AVERAGE WEIGHT")
+
+ggplot(rodent_sex_weight, aes(x=sex, y=mean_weight)) +
+  geom_col()+xlab("SEX") + ylab("AVERAGE WEIGHT")
+
+ggsave("figs/averageweightsex.png" ,mean_weight_bysex,width = 10, dpi=300)  
